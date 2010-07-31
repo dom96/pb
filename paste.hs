f142f4f371901f15a489b5a3a8d5b15d1360f18e
dom96 <dominikpicheta@googlemail.com>
2010-07-31 21:35:25 +0100

    Initial commit

---
+++ paste.hs
@@ -0,0 +1,40 @@
+import Network.Browser
+import Network.URI
+import Network.HTTP
+import Data.Maybe
+import System.IO
+import System.Process
+
+getGithubConfig :: IO (String, String)
+getGithubConfig = do
+    (pstdin, pstdout, pstderr, ph) <- runInteractiveCommand $ "git config --global github.user"
+    usr <- hGetLine pstdout
+    (pstdin, pstdout, pstderr, ph) <- runInteractiveCommand $ "git config --global github.token"
+    token <- hGetLine pstdout
+    return (usr, token)
+
+gistUrl = fromJust $ parseURI "http://gist.github.com/gists"
+testGist :: IO URI
+testGist = do
+    (uri, rsp) <- browse $ do
+                  setAllowRedirects True
+                  request $ formToRequest $
+                          Form POST gistUrl
+                                    [("file_ext[gistfile1]",".txt"),
+                                     ("file_name[gistfile1]", "test"),
+                                     ("file_contents[gistfile1]","hello world")]
+    return uri
+
+newGist :: String -> String -> String -> IO URI
+newGist contents filename ext = do
+    (usr, token) <- getGithubConfig
+    (uri, rsp) <- browse $ do
+                  setAllowRedirects True
+                  request $ formToRequest $
+                          Form POST gistUrl
+                                    [("file_ext[gistfile1]", ext),
+                                     ("file_name[gistfile1]", filename),
+                                     ("file_contents[gistfile1]", contents),
+                                     ("login", usr),
+                                     ("token", token)]
+    return uri
---
+++ readme.markdown
@@ -0,0 +1,2 @@
+# pb
+A simple pastebin application. It will hopefully support most of the major pastebin websites, gist, pastebin.com, dpaste
