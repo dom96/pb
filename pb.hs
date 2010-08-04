158df701fa057ff32cba58df9740652764c1102a
dom96 <dominikpicheta@googlemail.com>
2010-08-04 01:54:40 +0100

    Pasting from clipboard works now.

--- pb.hs
+++ pb.hs
@@ -1,11 +1,30 @@
 import System.IO
 import System.Environment
+import System.Process
 import Paste
 
+getClipboard :: IO String
+getClipboard = do
+    (pstdin, pstdout, pstderr, ph) <- runInteractiveCommand $ "xclip -o"
+    eof <- hIsEOF pstdout
+    if not $ eof
+        then hGetContents pstdout
+        else return ""
+
 parseArgs :: [String] -> IO ()
-parseArgs ["--help"] = do
-    putStrLn "Usage: pb [options]\n\
-             \pb --help          This help message"
+parseArgs (x:xs)
+    | x == "-c" || x == "--clipboard" = do
+        -- Get the contents of the clipboard
+        contents <- getClipboard
+        if not $ null contents
+            then do url <- newGist contents "test" ".txt"
+                    putStrLn $ show url
+            else putStrLn "Error: Nothing in the clipboard"
+        
+    | otherwise = do
+        putStrLn "Usage: pb [options]\n\
+                 \pb --help          This help message\n\
+                 \pb -c[--clipboard] Pastebin whatever is in the clipboard"
 parseArgs [] = do
     putStrLn "Paste the code you want to pastebin and press CTRL + D to pastebin."
     contents <- getContents
--- todo.markdown
+++ todo.markdown
@@ -1,4 +1,5 @@
 # todo
 * Add other pastebin sites, like pastebin.com
 * Add a way to execute code on pastebin sites, perhaps using ideone.com
-* Pastebin straight from the clipboard, use `xclip`
+* Pastebin straight from the clipboard, use `xclip` - **done**
+* Make a function for executing a command, and getting it's output.
